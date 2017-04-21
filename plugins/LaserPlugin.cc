63993c331c75bd592a752fa44e0ba1cf444556a4
emiliano gagliardi <emilianogagliardi@icloud.com>
2017-04-21 07:01:12 -0700

    laser sensor adapter completed

--- plugins/LaserPlugin.cc
+++ plugins/LaserPlugin.cc
@@ -43,15 +43,21 @@ void LaserPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
     // get ros node handler
     ros::NodeHandle node;
     
-    publisher = node.advertise<std_msgs::Bool>("/pallet_jack/load");
+    publisher = node.advertise<std_msgs::Bool>("/pallet_jack/load", 1);
     
     std::cout << "executed load method\n\n";
 }
 
 void LaserPlugin::OnUpdate()
 {
-    std::cout << "onUpdate\n\n";
-    bool is_loaded = (parentSensor->RangeMin() < THRESHOLD);
+    // get the status of the father sensor, and find the most near object distance
+    std::vector<double> ranges;
+    parentSensor->Ranges(ranges);
+    double min = ranges[0];
+    for(auto& x : ranges) {
+	if (x < min) min = x;
+    }
+    bool is_loaded = (min < THRESHOLD);
     std_msgs::Bool msg;
     msg.data = is_loaded;
     publisher.publish(msg);
